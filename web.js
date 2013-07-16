67aaa11332d325ca3fe64d29d8bf34fc0944043f
Ubuntu <ubuntu@ip-10-164-74-229.ec2.internal>
2013-07-16 05:57:38 +0000

    web.js w/index.html wo/ buffer

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    js.readFileSync('index.html');
+    fs.readFileSync('index.html');
 });
 
 var port = process.env.PORT || 5000;
