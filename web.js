c685af71ec671ab4fcc3ffb363b3ae7b44e6fd36
Luis Ricardo Sanchez <luissanchez0305@gmail.com>
2013-07-18 05:01:04 +0000

    modification web.js

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
-    fs.readFileSync('index.html');
+  response.send('Hello World 2!');
+//    fs.readFileSync('index.html');
 });
 
 var port = process.env.PORT || 5000;
